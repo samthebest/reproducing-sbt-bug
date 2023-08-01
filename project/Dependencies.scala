c2f36e84953231d7c0e498fc40af70d296f7745b
sam <sam@example.com>
2023-08-01 13:12:20 +0100

    Produces NPE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,22 +25,6 @@ object Dependencies {
     )
   }
 
-  object AWS {
-    val kinesisanalyticsRuntime: ModuleID = "com.amazonaws" % "aws-kinesisanalytics-runtime" % "1.2.0"
-    val kinesis: ModuleID = "software.amazon.awssdk" % "kinesis" % "2.15.78"
-    val sqs: ModuleID = "software.amazon.awssdk" % "sqs" % "2.15.78"
-    // Think only used by MRP
-    val s3: ModuleID = "software.amazon.awssdk" % "s3" % "2.15.78"
-    val ssm: ModuleID = "com.amazonaws" % "aws-java-sdk-ssm" % "1.12.435"
-
-    val all = Seq(kinesisanalyticsRuntime, kinesis, sqs, s3, ssm)
-  }
-
-  object TypeLevel {
-    val catsCore: ModuleID = "org.typelevel" %% "cats-core" % "2.1.0"
-    val squants: ModuleID = "org.typelevel" %% "squants" % "1.6.0"
-  }
-
   val scanamoVersion = "1.0.0-M15"
 
   object Misc {
